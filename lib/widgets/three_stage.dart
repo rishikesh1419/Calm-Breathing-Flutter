e3e8564d47a8a6e33071f9bf30419000026d1c85
rishikesh1419 <rishikesh1419@gmail.com>
2020-11-07 23:52:10 +0530

    4-7-8 done

--- lib/widgets/three_stage.dart
+++ lib/widgets/three_stage.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class ThreeStage extends StatefulWidget {
@@ -5,11 +7,48 @@ class ThreeStage extends StatefulWidget {
   _ThreeStageState createState() => _ThreeStageState();
 }
 
-class _ThreeStageState extends State<ThreeStage> {
+class _ThreeStageState extends State<ThreeStage> with TickerProviderStateMixin {
+  AnimationController _breathingController;
+  String _action = 'Breathe In';
+
+  @override
+  void initState() {
+    super.initState();
+    _breathingController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )
+    ..addStatusListener((status) {
+      if(status == AnimationStatus.completed) {
+        _action = 'Hold';
+        Timer(const Duration(milliseconds: 3500), () {
+          _breathingController.duration = const Duration(seconds: 4);
+          _action = 'Breathe Out';
+          _breathingController.reverse();
+        });
+      }
+      else if(status == AnimationStatus.dismissed) {
+        _breathingController.duration = const Duration(seconds: 2);
+        _action = 'Breathe In';
+        _breathingController.forward();
+      }
+    })
+    ..addListener(() {
+      setState(() {
+      });
+    });
+    _breathingController.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      height: 180.0 + 50* _breathingController.value,
+      decoration: BoxDecoration(
+      color: Colors.greenAccent,
+        shape: BoxShape.circle,
+      ),
+      child: Center(child: Text(_action)),
     );
   }
 }
