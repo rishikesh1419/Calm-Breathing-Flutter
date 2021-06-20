e4c7c0c06bace157733499506f4cf15715f255f9
Rishikesh Kadam <rishikesh1419@gmail.com>
2021-06-21 01:42:38 +0530

    Info screen done

--- lib/screens/info_screen.dart
+++ lib/screens/info_screen.dart
@@ -1,8 +1,7 @@
 import 'package:calm_breathing/utils/constants.dart';
 import 'package:calm_breathing/widgets/app_bar.dart';
-import 'package:flutter/gestures.dart';
+import 'package:calm_breathing/widgets/info_item.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class InfoScreen extends StatelessWidget {
   @override
@@ -18,64 +17,21 @@ class InfoScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              "4-7-8 Breathing",
-              style: TextStyle(
-                color: lightGreen,
-                fontSize: 22,
-              ),
-              textAlign: TextAlign.left,
+            InfoItem(
+              type: "7/11 Breathing",
+              details: "This breathing exercise can help you to reduce stress in the moment. " +
+                  "If you practice it regularly, you may also find that it helps you feel calmer generally. " +
+                  "The more you practice, the more effective this technique becomes.",
+              url:
+                  "https://thewellbeingthesis.org.uk/foundations-for-success/stress/711-breathing/",
             ),
-            SizedBox(height: 16.0),
-            const Text(
-              "The 4-7-8 technique forces the mind and body to focus on regulating" +
+            InfoItem(
+              type: "4-7-8 Breathing",
+              details: "The 4-7-8 technique forces the mind and body to focus on regulating" +
                   "the breath, rather than replaying your worries when you lie down at night." +
                   " Proponents claim it can soothe a racing heart or calm frazzled nerves." +
                   " Dr. Weil has even described it as a “natural tranquilizer for the nervous system.”",
-              textAlign: TextAlign.justify,
-              style: TextStyle(
-                color: lightGreen,
-                fontSize: 18,
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(
-                top: 12.0,
-                right: 8.0,
-              ),
-              alignment: Alignment.centerRight,
-              child: RichText(
-                textAlign: TextAlign.end,
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () async {
-                          var url =
-                              "https://www.healthline.com/health/4-7-8-breathing";
-                          if (await canLaunch(url)) {
-                            await launch(url);
-                          } else {
-                            throw 'Could not launch $url';
-                          }
-                        },
-                      text: "Source",
-                      style: TextStyle(
-                        color: lightGreen,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 16,
-                      ),
-                    ),
-                    WidgetSpan(
-                      child: Icon(
-                        Icons.open_in_new,
-                        color: lightGreen,
-                        size: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              url: "https://www.healthline.com/health/4-7-8-breathing",
             ),
           ],
         ),
---
+++ lib/widgets/info_item.dart
@@ -0,0 +1,82 @@
+import 'package:calm_breathing/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/gestures.dart';
+
+class InfoItem extends StatelessWidget {
+  final String type;
+  final String details;
+  final String url;
+
+  const InfoItem(
+      {Key key,
+      @required this.type,
+      @required this.details,
+      @required this.url})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            type,
+            style: TextStyle(
+              color: lightGreen,
+              fontSize: 22,
+            ),
+            textAlign: TextAlign.left,
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            details,
+            textAlign: TextAlign.justify,
+            style: TextStyle(
+              color: lightGreen,
+              fontSize: 18,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(
+              top: 12.0,
+              right: 8.0,
+            ),
+            alignment: Alignment.centerRight,
+            child: RichText(
+              textAlign: TextAlign.end,
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () async {
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          throw 'Could not launch $url';
+                        }
+                      },
+                    text: "Source",
+                    style: TextStyle(
+                      color: lightGreen,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 16,
+                    ),
+                  ),
+                  WidgetSpan(
+                    child: Icon(
+                      Icons.open_in_new,
+                      color: lightGreen,
+                      size: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
