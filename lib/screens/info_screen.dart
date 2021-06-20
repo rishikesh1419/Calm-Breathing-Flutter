a34e1ebb28d638a2b441d0bccccdd914e3a471ef
Rishikesh Kadam <rishikesh1419@gmail.com>
2021-06-20 21:26:15 +0530

    Info screen template

--- lib/screens/info_screen.dart
+++ lib/screens/info_screen.dart
@@ -1,3 +1,4 @@
+import 'package:calm_breathing/utils/constants.dart';
 import 'package:calm_breathing/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -5,9 +6,56 @@ class InfoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: black,
       appBar: MyAppBar(info: true),
-      body: Center(
-        child: const Text("Info Screen"),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 24.0,
+          horizontal: 24.0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "4-7-8 Breathing",
+              style: TextStyle(
+                color: lightGreen,
+                fontSize: 22,
+              ),
+              textAlign: TextAlign.left,
+            ),
+            SizedBox(height: 16.0),
+            const Text(
+              "The 4-7-8 technique forces the mind and body to focus on regulating" +
+                  "the breath, rather than replaying your worries when you lie down at night." +
+                  " Proponents claim it can soothe a racing heart or calm frazzled nerves." +
+                  " Dr. Weil has even described it as a “natural tranquilizer for the nervous system.”",
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                color: lightGreen,
+                fontSize: 18,
+              ),
+            ),
+            // Container(
+            //   width: double.infinity,
+            Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+              const Text(
+                "Source",
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  color: lightGreen,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 16,
+                ),
+              ),
+              Icon(
+                Icons.open_in_new,
+                color: lightGreen,
+                size: 16,
+              )
+            ])
+          ],
+        ),
       ),
     );
   }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,5 +4,6 @@ const appName = "Calm Breathing";
 const green = Colors.green;
 const black = Colors.black;
 const white = Colors.white;
+const lightGreen = Color.fromRGBO(161, 255, 178, 0.9);
 const greenAccent = Colors.greenAccent;
 const transparent = Colors.transparent;
